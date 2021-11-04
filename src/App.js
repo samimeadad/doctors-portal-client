607f79f32581dcad544ea06aed473c0c7b30bff0
Sami Meadad Choudhury <sami.meadad@gmail.com>
2021-11-04 13:43:54 +0600

    react router, react-router-dom, material-ui insatalled

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './Pages/Home/Home/Home';
+import ErrorPage from './Pages/ErrorPage/ErrorPage';
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+          <Route exact path="/home" element={ <Home /> } />
+          <Route exact path="/" element={ <Home /> } />
+          <Route path="*" element={ <ErrorPage /> } />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+const Home = () => {
+    return (
+        <div>
+            <h2>Home Page</h2>
+            <Button variant="contained">Hello World</Button>
+        </div>
+    );
+};
+
+export default Home;    
